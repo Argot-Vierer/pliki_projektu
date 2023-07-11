5fd5c456da55e70005f8597f0a0faaa10f217bd0
Argot <argot-vierer.0e@icloud.com>
2023-07-12 00:45:27 +0200

    to do list component

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class TodoListComponent {
 
+  todos: string[] = [];
+  addTodo(): void {
+    console.log('Aktualna lista todo:' , this.todos);
+  }
+
 }
